97a0f060a3e87bab113579829c82b3b917ab608c
Ryan Yocum <rtyocum1@gmail.com>
2026-04-16 13:25:56 -0400

    fix sonar

--- backend/db.sh
+++ backend/db.sh
@@ -2,7 +2,7 @@
 
 # this script checks if the database url is set in .env which overrides the container
 # if it is set, we will exit silently, otherwise we will start the container
-if [ -z "$DATABASE_URL" ]; then
+if [[ -z "$DATABASE_URL" ]]; then
   if ! pnpm run db:status 2>/dev/null | grep -q "trip-itinerary-planner-mongo"; then
     echo "Starting the database container..."
     pnpm run db:up
