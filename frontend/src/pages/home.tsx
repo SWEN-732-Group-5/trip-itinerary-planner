f5d7abf1530c8aa8887c8fad73b378da8688a664
Hiro <hiroto125takeuchi@gmail.com>
2026-04-21 01:48:57 -0400

    fix: disable dumb eslint rule

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,15 +1,9 @@
 import { Button } from '@/components/ui/button';
 import { UserTrips } from '@/components/user-trips';
 import { LoggedIn, LoggedOut, LogoutButton } from '@/lib/auth/authSession';
-import { Link, useNavigate } from 'react-router';
+import { Link } from 'react-router';
 
 export default function Home() {
-	const navigate = useNavigate();
-	const navigateToTrip = (formData: FormData) => {
-		const tripId = formData.get('id');
-		if (!tripId) return;
-		navigate(`/trips/${tripId}`);
-	};
 	return (
 		<div className="p-6">
 			<h1 className="text-2xl font-bold">Welcome to Trip Itinerary Planner</h1>
